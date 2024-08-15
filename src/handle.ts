7185b493205735e36666af12c3e5fcb61753475e
glinto <glinto@gmail.com>
2024-08-15 08:01:07 +0200

    update request handlers

--- src/handle.ts
+++ src/handle.ts
@@ -1,32 +1,37 @@
-import { readFile, stat } from "fs/promises";
-import { IncomingMessage, ServerResponse } from "http";
-import path from "path";
-import { log } from "./utils";
+import { readFile, stat } from 'fs/promises';
+import { IncomingMessage, ServerResponse } from 'http';
+import path from 'path';
 
-export interface VueezRouteHandler {
+import { statSync } from 'fs';
+import { log } from './utils.js';
+
+export interface RouteHandler {
 	handle(url: URL, res: ServerResponse<IncomingMessage>, req?: IncomingMessage): Promise<void>;
 }
 
-export class NotFoundHandler implements VueezRouteHandler {
-	handle(_url: URL, res: ServerResponse<IncomingMessage>): Promise<void> {
-		res.statusCode = 404;
-		res.end();
-		return Promise.resolve();
-	}
-}
-
-export class StaticURLHandler implements VueezRouteHandler {
-
+export class StaticHandler implements RouteHandler {
 	readonly mimeMappings: { [key: string]: string } = {
 		'.svg': 'image/svg+xml',
+		'.png': 'image/png',
+		'.jpg': 'image/jpeg',
+		'.jpeg': 'image/jpeg',
 		'.js': 'text/javascript',
 		'.css': 'text/css',
 		'.html': 'text/html',
 		'.json': 'application/json',
 		'.txt': 'text/plain'
-	}
+	};
 
-	constructor(private readonly rootPath: string, private readonly mapTo?: string) { }
+	constructor(
+		private readonly rootPath: string,
+		private readonly mapTo?: string
+	) {
+		this.rootPath = path.resolve(rootPath);
+		const stat = statSync(this.rootPath);
+		if (!stat.isDirectory()) {
+			throw new Error('Static path root must be a directory');
+		}
+	}
 
 	handle(url: URL, res: ServerResponse<IncomingMessage>): Promise<void> {
 		if (url.pathname === '/') return Promise.reject();
@@ -34,14 +39,12 @@ export class StaticURLHandler implements VueezRouteHandler {
 		// remove +semver from the pathname, e.g. stylle+1.2.3.css -> style.css
 		let filePath = url.pathname.replace(/\+\d+(\.\d+)*/, '');
 
-
 		if (this.mapTo !== undefined) {
 			// Check if filePath begins with mapTo
 			if (filePath.startsWith(this.mapTo)) {
 				//remove mapTo from the filePath start
 				filePath = filePath.slice(this.mapTo.length);
-			}
-			else {
+			} else {
 				return Promise.reject();
 			}
 		}
@@ -53,16 +56,16 @@ export class StaticURLHandler implements VueezRouteHandler {
 		}
 
 		return stat(staticPath)
-			.then(stats => {
+			.then((stats) => {
 				if (stats.isDirectory()) {
 					log('Rejecting static directory request', url.pathname);
 					return Promise.reject();
 				}
 				return readFile(staticPath);
 			})
-			.then(data => {
-				Object.keys(this.mimeMappings).some(key => {
-					if (staticPath.match(new RegExp(`.*\.${key}$`))) {
+			.then((data) => {
+				Object.keys(this.mimeMappings).some((key) => {
+					if (staticPath.match(new RegExp(`.*\\${key}$`))) {
 						res.setHeader('Content-Type', this.mimeMappings[key] as string);
 						return true;
 					}
